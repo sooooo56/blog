c77f6889de5c2308880ee59bb0b5ebe083bd1138
sooooo56 <sooooo4645@gmail.com>
2024-10-29 21:48:59 +0900

    default note init

---
+++ src/main/java/com/korea/blog/domain/main/MainService.java
@@ -0,0 +1,29 @@
+package com.korea.blog.domain.main;
+
+import com.korea.blog.domain.main.note.entity.Note;
+import com.korea.blog.domain.main.note.repository.NoteRepository;
+import com.korea.blog.domain.main.note.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MainService {
+
+    private final NoteService noteService;
+
+    public void init(){
+        List<Note> noteList = noteService.getList();
+
+        if (getNoteList().isEmpty()){
+            noteService.saveDefault();
+        }
+    }
+
+    public List<Note> getNoteList() {
+
+        return noteService.getList();
+    }
+}
---
+++ src/main/java/com/korea/blog/domain/main/note/service/NoteService.java
@@ -0,0 +1,27 @@
+package com.korea.blog.domain.main.note.service;
+
+import com.korea.blog.domain.main.note.entity.Note;
+import com.korea.blog.domain.main.note.repository.NoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public List<Note> getList() {
+        return noteRepository.findAll();
+    }
+
+    public Note saveDefault() {
+        Note note = Note.builder()
+                .title("New Title")
+                .content("")
+                .build();
+
+        return noteRepository.save(note);
+    }
+}
